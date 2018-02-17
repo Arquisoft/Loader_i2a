f0e997f12beb1749bb5292a0c3714e4d75799036
Lorena Castillero <uo252358@uniovi.es>
2018-02-17 20:01:05 +0100

    Added file to tests

--- src/test/java/main/asw/UserTest.java
+++ src/test/java/main/asw/UserTest.java
@@ -1,13 +1,17 @@
 package main.asw;
 
-import main.asw.user.GeoCords;
-import main.asw.user.User;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.junit.Test;
+
+import main.asw.user.GeoCords;
+import main.asw.user.User;
 
 /**
  * Created by nicolas on 18/02/17 for citizensLoader0.
@@ -21,6 +25,20 @@ public class UserTest {
         date = df.parse(birthDateString);
         return date;
     }
+    
+    @Test
+	public void testCorrectUser() throws ParseException {
+		GeoCords gc = new GeoCords(43.3619, 5.8494);
+		User u = new User("Juan Aza", gc, "juan@gmail.com", "71678798B", 1);
+		assertEquals(u.getName(), "Juan Aza");
+		assertEquals(u.getEmail(), "juan@gmail.com");
+		assertEquals(u.getKind(), 1);
+		assertEquals(u.getLocation(), gc.toString());
+		assertEquals(u.toString(), "User [name=" + u.getName() + ", location=" + gc.toString() + ", email="
+				+ u.getEmail() + ", identifier=" + u.getIdentifier() + ", kind=" + u.getKind() + "]");
+		assertTrue(gc.getLat() == 43.3619);
+		assertTrue(gc.getLng() == 5.8494);
+	}
 
     @Test(expected = IllegalArgumentException.class)
     public void testBadEmail() throws ParseException {
--- src/test/java/main/asw/parser/ParserTest.java
+++ src/test/java/main/asw/parser/ParserTest.java
@@ -22,6 +22,7 @@ public class ParserTest {
     private final static String TEST_NO_FOUND_FILE = "noExiste";
     private static final String TEST_LESS_LINES = "lessLines.xls";
     private static final String TEST_MORE_LINES = "moreLines.xls";
+    private static final String TEST_WITH_ERRORS = "test-errors.xls";
 
 
     private ParserImpl parser;
@@ -98,6 +99,12 @@ public class ParserTest {
         parser = ParserFactory.getParser(BASE_PATH + TEST_LESS_LINES);
         parser.readList();
         assertEquals(0, parser.getUsers().size());
+    }
+    
+    @Test
+    public void testWithParseErrors() throws IOException, ParseException {
+        parser = ParserFactory.getParser(BASE_PATH + TEST_WITH_ERRORS);
+        parser.readList();
     }
 
 /* Not needed. It's being tested on PersistenceTest class */
